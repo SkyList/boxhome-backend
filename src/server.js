acd8dbb62949cb9699c7439dd8fe8d9221d8cfde
MATHEUS PANTOJA <matheus.pantoja97@gmail.com>
2019-04-14 22:00:19 -0300

    web socket resolvido

--- src/server.js
+++ src/server.js
@@ -1,36 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const cors = require('cors');
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-app.use(cors());
+app.use(cors())
 
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
 io.on('connection', socket => {
 	socket.on('connectRoom', box => {
-		socket.join(box);
+		socket.join(box)
 	})
-	console.log("ok");
+	console.log('Ok!')
 })
 
 mongoose.connect('mongodb://pantoja:123qwe@ds159747.mlab.com:59747/homebox', {
 	useNewUrlParser: true
 })
 
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(require('./routes'))
-app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
-
 app.use((req, res, next) => {
-	req.io = io;
-	return next();
+	req.io = io
+	return next()
 })
 
-server.listen(process.env.PORT || 3333);
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(require('./routes'))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
+
+server.listen(process.env.PORT || 3333)
